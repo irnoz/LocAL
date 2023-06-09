5b9cb7eb51777d02fb46706099f8415c705d783c
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-09 14:22:14 +0400

    core data set up

--- LocAL/Functions.swift
+++ LocAL/Functions.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+let applicationDocumentsDirectory: URL = {
+  let paths = FileManager.default.urls(
+    for: .documentDirectory,
+    in: .userDomainMask)
+  return paths[0]
+}()
+
 func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
   DispatchQueue.main.asyncAfter(
     deadline: .now() + seconds,
