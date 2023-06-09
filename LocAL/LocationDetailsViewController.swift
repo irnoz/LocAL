5bbb1b56095f47124463e2653ad2d733aa5d36ef
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-09 16:47:40 +0400

    core location editeing items

--- LocAL/LocationDetailsViewController.swift
+++ LocAL/LocationDetailsViewController.swift
@@ -32,12 +32,28 @@ class LocationDetailsViewController: UITableViewController {
   var categoryName = "No Category"
   var date = Date()
   var managedObjectContext: NSManagedObjectContext!
+  var descriptionText = ""
+  var locationToEdit: Location? {
+    didSet {
+      if let location = locationToEdit {
+        descriptionText = location.locationDescription
+        categoryName = location.category
+        date = location.date
+        coordinate = CLLocationCoordinate2DMake(
+          location.latitude,
+          location.longitude)
+        placemark = location.placemark
+      } }
+  }
 
   // MARK: - Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    if let location = locationToEdit {
+      title = "Edit Location"
+    }
     
-    descriptionTextView.text = ""
+    descriptionTextView.text = descriptionText
     categoryLabel.text = categoryName
     
     latitudeLabel.text = String(
@@ -95,9 +111,16 @@ class LocationDetailsViewController: UITableViewController {
   @IBAction func done() {
     guard let mainView = navigationController?.parent?.view else { return }
     let hudView = HudView.hud(inView: mainView, animated: true)
-    hudView.text = "Tagged"
+
+    let location: Location
+    if let temp = locationToEdit {
+      hudView.text = "Updated"
+      location = temp
+    } else {
+      hudView.text = "Tagged"
+      location = Location(context: managedObjectContext)
+    }
     
-    let location = Location(context: managedObjectContext)
     location.locationDescription = descriptionTextView.text
     location.category = categoryName
     location.latitude = coordinate.latitude
--- LocAL/LocationsViewController.swift
+++ LocAL/LocationsViewController.swift
@@ -13,23 +13,34 @@ class LocationsViewController: UITableViewController {
   var managedObjectContext: NSManagedObjectContext!
   var locations = [Location]()
   
-  // MARK: - Life Cycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
+  lazy var fetchedResultsController:
+  NSFetchedResultsController<Location> = {
     let fetchRequest = NSFetchRequest<Location>()
     let entity = Location.entity()
     fetchRequest.entity = entity
-    
     let sortDescriptor = NSSortDescriptor(
       key: "date",
       ascending: true)
     fetchRequest.sortDescriptors = [sortDescriptor]
-    do {
-      locations = try managedObjectContext.fetch(fetchRequest)
-    } catch {
-      fatalCoreDataError(error)
-    }
+    fetchRequest.fetchBatchSize = 20
+    let fetchedResultsController = NSFetchedResultsController(
+      fetchRequest: fetchRequest,
+      managedObjectContext: self.managedObjectContext,
+      sectionNameKeyPath: nil,
+      cacheName: "Locations")
+    fetchedResultsController.delegate = self
+    return fetchedResultsController
+  }()
+  
+  deinit {
+    fetchedResultsController.delegate = nil
+  }
+  
+  // MARK: - Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    performFetch()
   }
   
   // MARK: - Table View Data Source
@@ -37,7 +48,8 @@ class LocationsViewController: UITableViewController {
     _ tableView: UITableView,
     numberOfRowsInSection section: Int
   ) -> Int {
-    return locations.count
+    let sectionInfo = fetchedResultsController.sections![section]
+    return sectionInfo.numberOfObjects
   }
   
   override func tableView(
@@ -46,26 +58,121 @@ class LocationsViewController: UITableViewController {
   ) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(
       withIdentifier: "LocationCell",
-      for: indexPath)
-    let location = locations[indexPath.row]
-    let descriptionLabel = cell.viewWithTag(100) as! UILabel
-    descriptionLabel.text = location.locationDescription
-    let addressLabel = cell.viewWithTag(101) as! UILabel
-    if let placemark = location.placemark {
-      var text = ""
-      if let tmp = placemark.subThoroughfare {
-        text += tmp + " "
-      }
-      if let tmp = placemark.thoroughfare {
-        text += tmp + ", "
-      }
-      if let tmp = placemark.locality {
-        text += tmp
-      }
-      addressLabel.text = text
-    } else {
-      addressLabel.text = ""
-    }
+      for: indexPath) as! LocationCell
+    let location = fetchedResultsController.object(at: indexPath)
+    cell.configure(for: location)
     return cell
   }
+  
+  // MARK: - Table View Delegate
+  override func tableView(
+    _ tableView: UITableView,
+    commit editingStyle: UITableViewCell.EditingStyle,
+    forRowAt indexPath: IndexPath
+  ) {
+    if editingStyle == .delete {
+      let location = fetchedResultsController.object(
+        at: indexPath)
+      managedObjectContext.delete(location)
+      do {
+        try managedObjectContext.save()
+      } catch {
+        fatalCoreDataError(error)
+      }
+    }
+  }
+  
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "EditLocation" {
+      let controller = segue.destination  as! LocationDetailsViewController
+      controller.managedObjectContext = managedObjectContext
+      if let indexPath = tableView.indexPath(
+        for: sender as! UITableViewCell) {
+        let location = fetchedResultsController.object(at: indexPath)
+        controller.locationToEdit = location
+      }
+    }
+  }
+  
+  // MARK: - Helper methods
+  func performFetch() {
+    do {
+      try fetchedResultsController.performFetch()
+    } catch {
+      fatalCoreDataError(error)
+    }
+  }
+}
+
+// MARK: - NSFetchedResultsController Delegate Extension
+extension LocationsViewController: NSFetchedResultsControllerDelegate {
+  func controllerWillChangeContent(
+    _ controller: NSFetchedResultsController<NSFetchRequestResult>
+  ){
+      print("*** controllerWillChangeContent")
+      tableView.beginUpdates()
+  }
+  
+  func controller(
+    _ controller: NSFetchedResultsController<NSFetchRequestResult>,
+    didChange anObject: Any,
+    at indexPath: IndexPath?,
+    for type: NSFetchedResultsChangeType,
+    newIndexPath: IndexPath?
+  ){
+    switch type { case .insert:
+      print("*** NSFetchedResultsChangeInsert (object)")
+      tableView.insertRows(at: [newIndexPath!], with: .fade)
+    case .delete:
+      print("*** NSFetchedResultsChangeDelete (object)")
+      tableView.deleteRows(at: [indexPath!], with: .fade)
+    case .update:
+      print("*** NSFetchedResultsChangeUpdate (object)")
+      if let cell = tableView.cellForRow(
+        at: indexPath!) as? LocationCell {
+        let location = controller.object(
+          at: indexPath!) as! Location
+        cell.configure(for: location)
+      }
+    case .move:
+      print("*** NSFetchedResultsChangeMove (object)")
+      tableView.deleteRows(at: [indexPath!], with: .fade)
+      tableView.insertRows(at: [newIndexPath!], with: .fade)
+    @unknown default:
+      print("*** NSFetchedResults unknown type")
+    }
+  }
+  
+  func controller(
+    _ controller:
+    NSFetchedResultsController<NSFetchRequestResult>,
+    didChange sectionInfo: NSFetchedResultsSectionInfo,
+    atSectionIndex sectionIndex: Int,
+    for type: NSFetchedResultsChangeType
+  ) {
+    switch type {
+    case .insert:
+      print("*** NSFetchedResultsChangeInsert (section)")
+      tableView.insertSections(
+        IndexSet(integer: sectionIndex), with: .fade)
+    case .delete:
+      print("*** NSFetchedResultsChangeDelete (section)")
+      tableView.deleteSections(
+        IndexSet(integer: sectionIndex), with: .fade)
+    case .update:
+      print("*** NSFetchedResultsChangeUpdate (section)")
+    case .move:
+      print("*** NSFetchedResultsChangeMove (section)")
+    @unknown default:
+      print("*** NSFetchedResults unknown type")
+    }
+  }
+  
+  func controllerDidChangeContent(
+    _ controller:
+    NSFetchedResultsController<NSFetchRequestResult> ){
+      print("*** controllerDidChangeContent")
+      tableView.endUpdates()
+    }
 }
